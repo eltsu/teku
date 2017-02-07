5f5d6a630a8c9be560c40c8ce5c81ac936751821
elts <elena.olsamo@myy.haaga-helia.fi>
2017-02-07 13:52:13 +0200

    Harjoitus 3 complete

--- Bookstore/src/main/java/com/jatkuva/elena/domain/Book.java
+++ Bookstore/src/main/java/com/jatkuva/elena/domain/Book.java
@@ -3,14 +3,26 @@ package com.jatkuva.elena.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
 public class Book {
-	private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private long id;
 	private String title;
 	private String author;
 	private int year;
 	private String isbn;
 	private Double price;
+
 	public Book(Long id, String title, String author, int year, String isbn, Double price) {
 		super();
 		this.id = id;
@@ -20,8 +32,20 @@ public class Book {
 		this.isbn = isbn;
 		this.price = price;
 	}
+	
+
 	public Book() {
 		super();
 	}
-	
+
+
+	public Book(String title, String author, int year, String isbn, Double price) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		this.isbn = isbn;
+		this.price = price;
+	}
+
 }
